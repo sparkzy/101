b77d76c35e004e371cde6bdf8e00bdb103facb3d
Bobby M <31968082+sparkzy@users.noreply.github.com>
2018-03-08 11:22:43 -0500

    Adding Angular

---
+++ Angular/project/src/app/components/flashcard-set/flashcard-set.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flashcard-set',
+  templateUrl: './flashcard-set.component.html',
+  styleUrls: ['./flashcard-set.component.css']
+})
+export class FlashcardSetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
