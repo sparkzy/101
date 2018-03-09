b7c2e3b9c56c36f78a2721a73e34a67427a362ce
Charles Courtois <ccourtois4295@gmail.com>
2018-03-09 14:50:42 -0500

    slightly modified various files

--- 101-boot/src/main/java/com/revature/repos/SubjectRepo.java
+++ 101-boot/src/main/java/com/revature/repos/SubjectRepo.java
@@ -1,17 +1,13 @@
 package com.revature.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import com.revature.entities.Status;
 import com.revature.entities.Subject;
 
 public interface SubjectRepo extends JpaRepository<Subject, Integer> {
-	
-	
-	Subject findBysubjectId(int id);
-	Subject deleteBysubjectId(int id);
 
-	
+	Subject findBysubjectId(int id);
+
+	Subject deleteBysubjectId(int id);
 
 }
--- 101-boot/src/main/java/com/revature/services/StatusServiceImpl.java
+++ 101-boot/src/main/java/com/revature/services/StatusServiceImpl.java
@@ -1,28 +1,26 @@
 package com.revature.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import com.revature.entities.Status;
 import com.revature.repos.StatusRepo;
+
 @Service
 public class StatusServiceImpl implements ServiceInterface<Status> {
 	@Autowired
 	private StatusRepo statRepo;
 
 	@Override
-	@Transactional 
+	@Transactional
 	public Status save(Status s) {
 		return statRepo.save(s);
 	}
 
-	
 	@Override
 	@Transactional
 	public Status findById(int id) {
@@ -32,25 +30,22 @@ public class StatusServiceImpl implements ServiceInterface<Status> {
 	@Override
 	@Transactional
 	public List<Status> findAll() {
-		
-		 return statRepo.findAll();
+
+		return statRepo.findAll();
 	}
 
 	@Override
 	@Transactional
 	public void update(Status s) {
-		 statRepo.save(s);
-		
+		statRepo.save(s);
+
 	}
 
 	@Override
 	@Transactional
 	public void delete(int id) {
 		statRepo.deleteById(id);
-		
+
 	}
 
-	
-
-	
 }
