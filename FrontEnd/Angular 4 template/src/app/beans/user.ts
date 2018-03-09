4455b7d0f333651255ff9259668294e944d9bee7
Bobby M <31968082+sparkzy@users.noreply.github.com>
2018-03-09 10:46:39 -0500

    Deleted Front End/Angular Template

--- FrontEnd/Angular 4 template/src/app/beans/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  id: number;
-  roleid: number;
-  username: string;
-  password: string;
-  firstname: string;
-  lastname: string;
-  email: string;
-}
--- FrontEnd/Angular 4 template/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  context: 'http://localhost:8080/expense-reimbursement-system/'
-};
